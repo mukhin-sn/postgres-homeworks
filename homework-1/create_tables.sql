1319b5f2854a3072c2acbc14a891bbb9b6da38c7
Sergey Mukhin <senikomu@gmail.com>
2023-12-10 20:38:09 +0700

    The SQL code for creating tables is copied to the create_tables.sql file

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(50),
+	last_name varchar(50),
+	title varchar(50),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id char(5),
+	company_name varchar(100),
+	contact_name varchar(50)
+);
+
+CREATE TABLE orders
+(
+	order_id int,
+	customer_id char(5),
+	employee_id int,
+	order_date date,
+	ship_city varchar(50)
+)
