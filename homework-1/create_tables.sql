8c15f571bf9bd93e4d751b70299dfa8f297d0155
mukhin-sn <senikomu@gmail.com>
2023-12-11 02:25:14 +0700

    A connection has been established between orders and employees. A connection has been established between orders and customers.

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -3,7 +3,7 @@
 CREATE TABLE employees
 (
 	employee_id int PRIMARY KEY,
-	first_name varchar(50),
+	first_name varchar(50) NOT NULL,
 	last_name varchar(50),
 	title varchar(50),
 	birth_date date,
@@ -12,16 +12,16 @@ CREATE TABLE employees
 
 CREATE TABLE customers
 (
-	customer_id char(5),
-	company_name varchar(100),
+	customer_id char(5) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
 	contact_name varchar(50)
 );
 
 CREATE TABLE orders
 (
-	order_id int,
-	customer_id char(5),
-	employee_id int,
+	order_id int PRIMARY KEY,
+	customer_id char(5) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
 	order_date date,
-	ship_city varchar(50)
+	ship_city varchar(50) NOT NULL
 )
